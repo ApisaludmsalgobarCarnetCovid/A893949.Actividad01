3b81a46e5b833a67e4faf364dba051ce753929ab
francolonca <36172650+francolonca@users.noreply.github.com>
2021-03-18 10:59:10 -0300

    Actualizacion

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _A893949.Actividad01_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!\nIngrese una tecla para que finalice el programa");
+            Console.ReadKey();
+        }
+    }
+}
